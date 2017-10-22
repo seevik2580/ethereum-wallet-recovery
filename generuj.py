38c343c8f3129797bc3bcd5203766777cc1a5b8b
seevik2580 <seevik@seznam.cz>
2017-10-22 05:30:53 +0200

    arguments min max ascii
    minlen (-min)
    maxlen (-max)
    ascii (-a)

--- generuj.py
+++ generuj.py
@@ -8,6 +8,10 @@ parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group(required=True)   
 group.add_argument("-s", "--slova", type=str, required=False, help="slova ze kterych generovat wordlist, oddeluj carkou")
 group.add_argument("-v", "--vstupnisoubor", type=str, required=False, help="soubor ve kterem jsou slova ze kterych generovat wordlist")
+parser.add_argument("-min", "--minimal", type=int, required=False, help="minimalni delka znaku")
+parser.add_argument("-max", "--maximal", type=int, required=False, help="maximalni delka znaku")
+group.add_argument("-a", "--ascii", action='store_true', required=False, help="generate from ascii table")
+
 args = parser.parse_args() 
 
 class RotatingFile(object):
@@ -56,6 +60,11 @@ class RotatingFile(object):
 if __name__=='__main__':
     print "Generuji variace hesel"
     myfile = RotatingFile(max_files=99999999)
+    
+    if args.ascii is True:
+	    f="0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,!,\",#,$,%,&,',(,),*,+,-,.,/,:,;,<,=,>,?,@,[,\,],^,_,`,{,|,},~, "
+	    slova=f
+	
     if args.slova is not None:
         f=args.slova
         slova=f
@@ -64,17 +73,29 @@ if __name__=='__main__':
         f=open(args.vstupnisoubor)
         slova=f.readline()
         
+    if args.minimal is None:
+	    minimal = 1
+    else:
+	    minimal = args.minimal
+
+    if args.maximal is None:
+	    maximal = 64
+    else:
+    	maximal = args.maximal
+
     while not myfile.finished:
         maxpocet=len(slova.split(","))
         for pocet in xrange(maxpocet+1):
             for group in permutations(slova.split(","), pocet):
-              slovo=''.join(group)
-              delka=len(slovo)
-              myfile.c+=1
-              myfile.write("%s\n" % slovo)
-              if myfile.c % 100000 == 0:
-                print "Generuji", myfile.c,". heslo" 
-                
+                slovo=''.join(group)
+                delka=len(slovo)
+                if delka >= minimal and delka <= maximal:
+                    myfile.c+=1
+                    myfile.write("%s\n" % slovo)
+                    if myfile.c % 100000 == 0:
+                        print "Generuji", myfile.c,". heslo" 
+                else:
+                    break
         
         print myfile.filename_template, time.time() - myfile.time1
         print "Hotovo, vygenerovano %s kombinaci" % myfile.c
